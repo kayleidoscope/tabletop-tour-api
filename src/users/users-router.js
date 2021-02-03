073a948df6cd9ca29c5cf919b6fd797b3c6f44df
Kay Holten <holtenkay@gmail.com>
2021-02-03 13:04:48 -0800

    adds user migration, router, and service object

---
+++ src/users/users-router.js
@@ -0,0 +1,102 @@
+const path = require('path')
+const express = require('express');
+const xss = require('xss')
+const usersRouter = express.Router();
+const UsersService = require('./users-service.js');
+const jsonParser = express.json()
+
+usersRouter
+    .route('/')
+    .get((req, res, next) => {
+        const knexInstance = req.app.get('db')
+        UsersService.getAllUsers(knexInstance)
+            .then(users => {
+                res.json(users)
+            })
+            .catch(next)
+    })
+    .post(jsonParser, (req, res, next) => {
+        const {username} = req.body
+        const newUser = {username}
+
+        if (!username) {
+            return res.status(400).json({
+                error: {message: 'You must provide a username'}
+            })
+        }
+
+        UsersService.insertUser(
+            req.app.get('db'),
+            newUser
+        )
+            .then(user => {
+                res
+                    .status(201)
+                    .location(path.posix.join(req.originalUrl, `/${user.id}`))
+                    .json({
+                        id: user.id,
+                        username: xss(user.username),
+                        acct_created: user.acct_created
+                    })
+            })
+            .catch(next)
+    })
+
+usersRouter
+    .route('/:id')
+    .all((req, res, next) => {
+        const {id} = req.params;
+        const knexInstance = req.app.get('db')
+        UsersService.getById(knexInstance, id)
+            .then(user => {
+                if(!user) {
+                    return res.status(404).json({
+                        error: {message: `User doesn't exist`}
+                    })
+                }
+                res.user = user //save the user for the next middleware
+                next()
+            })
+            .catch(next)
+    })
+    .get((req, res, next) => {
+        res.json({
+            id: res.user.id,
+            username: xss(res.user.username),
+            acct_created: res.user.acct_created
+        })
+    })
+    .delete((req, res, next) => {
+        UsersService.deleteUser(
+            req.app.get('db'),
+            req.params.id
+        )
+            .then(() => {
+                res.status(204).end()
+            })
+            .catch(next)
+    })
+    .patch(jsonParser, (req, res, next) => {
+        const {username} = req.body
+        const userToUpdate = {username}
+
+        if(!username) {
+            return res.status(400).json({
+                error: {
+                    message: `You must provide a new username to change your username`
+                }
+            })
+        }
+
+        UsersService.updateUser(
+            req.app.get('db'),
+            req.params.id,
+            userToUpdate
+        )
+            .then(numRowsAffected => {
+                res.status(204).end()
+            })
+            .catch(next)
+    })
+
+module.exports = usersRouter
